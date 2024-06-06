6c268a7eaf544ce98a30082d3f52ccd56d50c5c4
Nischal Patel <nischalpatel20@gmail.com>
2024-06-06 23:38:25 +0530

    Update InterfaceSegregationPrinciple.go

--- LLD/SOLID/InterfaceSegregationPrinciple.go
+++ LLD/SOLID/InterfaceSegregationPrinciple.go
@@ -1,4 +1,5 @@
-//The Interface Segregation Principle (ISP) states that clients should not be forced to depend on interfaces they do not use. Instead of one large, general-purpose interface, multiple smaller, more specific interfaces should be created. This allows clients to depend only on the methods that are relevant to them.
+// The Interface Segregation Principle (ISP) states that clients should not be forced to depend on interfaces they do not use.
+// Instead of one large, general-purpose interface, multiple smaller, more specific interfaces should be created. This allows clients to depend only on the methods that are relevant to them.
 package main
 
 import "fmt"
