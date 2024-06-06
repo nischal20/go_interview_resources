de3fd37975c5d92eb926c1151c8482177c358f4a
Nischal Patel <nischalpatel20@gmail.com>
2024-06-06 23:38:07 +0530

    Update LiskovSubstitutionPrinciple.go

--- LLD/SOLID/LiskovSubstitutionPrinciple.go
+++ LLD/SOLID/LiskovSubstitutionPrinciple.go
@@ -1,4 +1,5 @@
-//Liskov Substitution Principle (LSP) states that objects of a superclass should be replaceable with objects of a subclass without affecting the correctness of the program. In other words, if S is a subtype of T, then objects of type T may be replaced with objects of type S without altering any of the desirable properties of the program 
+// Liskov Substitution Principle (LSP) states that objects of a superclass should be replaceable with objects of a subclass without affecting the correctness of the program. 
+// In other words, if S is a subtype of T, then objects of type T may be replaced with objects of type S without altering any of the desirable properties of the program 
 package main
 
 import "fmt"
