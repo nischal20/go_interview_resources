ec9b396c81a6bce2c109181bec26b9464263fb9f
Nischal Patel <nischalpatel20@gmail.com>
2024-06-20 10:20:07 +0530

    Update Singelton.go

--- LLD/DesignPatterns/Singelton.go
+++ LLD/DesignPatterns/Singelton.go
@@ -1,31 +1,34 @@
-//In summary, the singleton pattern ensures that there is only one instance of the singleton struct. When you modify the data through one reference (s1), it is reflected in all other references (s2), since they all point to the same instance.
 package main
 
-
 import (
-  "fmt"
-  "sync"
+	"fmt"
+	"sync"
 )
-type Singleton struct {
-  data string
-}
 
+type Singleton struct {
+	data string
+}
 
 var instance *Singleton
 var mu sync.Mutex
+
+// Changed method receiver from (s *Singleton) to () to make it a package-level function
 func GetInstance() *Singleton {
-  mu.Lock()
-  defer mu.Unlock()
-  if instance == nil {
-     instance = Singleton{data: "daata"}
-  }
-  return instance
+	mu.Lock()
+	defer mu.Unlock()
+	if instance == nil {
+		instance = &Singleton{data: "data"}
+	}
+	return instance
 }
 
-
 func main() {
-  a := GetInstance()
-  fmt.Println(a.data)
-  b := GetInstance()
-  fmt.Println(b.data)
+	// Access the singleton instance directly through the package-level function
+	fmt.Println(GetInstance().data)
+	b := GetInstance()
+	fmt.Println(b.data)
+
+	// Modifying the singleton data to demonstrate it is the same instance
+	b.data = "new data"
+	fmt.Println(GetInstance().data)
 }
